771ea6b79d6f8967a355d80debb42d77ac3feb11
Jon Szymaniak <jon.szymaniak.foss@gmail.com>
2020-10-29 20:59:00 -0500

    Change README.md to a symlink
    By creating a repository containing multiple components, one of which
    is a Python package, I seem to have shot myself in the foot. Figures.
    
    (Yes, git subrepos would have been an appropriate technical solution,
    but this was not an option to me at the time.)
    
    The problem is as follows:
    
    We want the package hosted on PyPI to have a "long description" that
    always matches the Depthcharge project's top-level README.md.
    Previously, I had simply been loading this via a relative path (to the
    parent directory) from within setup.py.
    
    This was all well and good until I discovered that this effectively
    broke document generation with ReadTheDocs.io; the doc build operation
    is staged in a working directory assuming that setup.py resides in the
    root; attempting to access ../README.md does not point where we expect.
    
    I do not want to maintain two copies of the readme, nor remember to
    run long_desc.py each time the README.md is touched. This changeset
    shimmies things around with symlinks, which is Gross But Sufficient.
    
    There are bigger fish to try. Calling this good enough to close #14.
    We can address the next problem this causes when it rears its ugly head.

--- python/setup.py
+++ python/setup.py
@@ -43,95 +43,17 @@ def get_scripts() -> list:
     return ret
 
 
-# FIXME: Copy-paste of README in order to work around PIP/RTD issue of my own making.
-# https://github.com/nccgroup/depthcharge/issues/14
-#
+def get_description() -> str:
+    with open('../README.md', 'r') as infile:
+        return infile.read()
 
-_LONG_DESC = """\
-# Depthcharge
-
-<img align="right" src="https://raw.githubusercontent.com/nccgroup/depthcharge/master/doc/images/depthcharge-500.png" height="265" width="265">
-
-## What is Depthcharge?
-
-Depthcharge is a toolkit designed to support security research and
-"jailbreaking" of embedded platforms using the [U-Boot] bootloader.
-It consists of:
-
-* An extensible Python 3 [depthcharge module]
-* [Python scripts] built atop of the `depthcharge` module API
-* [Depthcharge "Companion" firmware], which is used to perform attacks requiring a malicious peripheral device.
-* Some example ["helper" payload binaries] and build scripts to get you started with U-Boot "standalone" program-esque payloads.
-
-[U-Boot]: https://www.denx.de/wiki/U-Boot
-[depthcharge module]: https://github.com/nccgroup/depthcharge/tree/master/python/depthcharge
-[Python scripts]: https://github.com/nccgroup/depthcharge/tree/master/python/scripts
-[Depthcharge "Companion" firmware]: https://github.com/nccgroup/depthcharge/tree/master/firmware/Arduino
-["helper" payload binaries]: https://github.com/nccgroup/depthcharge/tree/master/payloads
-["standalone"]: https://gitlab.denx.de/u-boot/u-boot/-/blob/v2020.01/doc/README.standalone
-
-
-## Project Documention
-
-More information can be found in the [online documentation] for the Depthcharge project.
-
-* [Introduction](https://depthcharge.readthedocs.io/en/latest/introduction.html)
-  * [What is Depthcharge?](https://depthcharge.readthedocs.io/en/latest/introduction.html#what-is-depthcharge)
-  * [Will this be useful for my situation?](https://depthcharge.readthedocs.io/en/latest/introduction.html#will-this-be-useful-for-my-situation)
-  * [What are some of its key features?](https://depthcharge.readthedocs.io/en/latest/introduction.html#what-are-some-of-its-key-features)
-  * [How do I get started?](https://depthcharge.readthedocs.io/en/latest/introduction.html#how-do-i-get-started)
-* [Python Scripts](https://depthcharge.readthedocs.io/en/latest/scripts/index.html)
-* [Python API](https://depthcharge.readthedocs.io/en/latest/api/index.html)
-* [Companion Firmware](https://depthcharge.readthedocs.io/en/latest/companion_fw.html)
-* [Troubleshooting](https://depthcharge.readthedocs.io/en/latest/troubleshooting.html)
-
-
-If you'd like to build this documentation for offline viewing, You can find the
-[Sphinx]-based documentation "source" in the [doc] directory.
-
-[doc]: https://github.com/nccgroup/depthcharge/tree/master/doc
-[online documentation]: https://depthcharge.readthedocs.io
-[Sphinx]: https://www.sphinx-doc.org/en/master/
-
-## Versioning
-
-Depthcharge uses a [Semantic versioning] scheme for both the Python API and the Companion Firmware.
-The version number for published releases will follow that of the Python API version.
-The [CHANGELOG] shall document the current version state of both, along
-with any compatibility information.
-
-Currently, this project uses ["unstable"] version numbers; API-breaking changes
-may occur within this minor version series, if deemed to be sufficiently
-beneficial for the future of the project. Refer to the
-CHANGELOG for guidance on handling any API changes.
-
-Each published release will have a "codename". This serves no real purpose,
-other than to amuse the author and add a little fun to preparing releases.
-(Maybe they'll even be useful to remember!) The codenames are song titles from
-punk bands, increasing alphabetically with each release.
-
-[CHANGELOG]: https://github.com/nccgroup/depthcharge/blob/master/CHANGELOG
-[Semantic versioning]: https://semver.org
-["unstable"]: https://semver.org/#spec-item-4
-
-## License
-
-All Depthcharge components are licensed under the [BSD 3-Clause License],
-found in the [License.txt] file. Project files use the corresponding
-[SPDX Identifier] to denote this.
-
-[BSD 3-Clause License]: https://opensource.org/licenses/BSD-3-Clause
-[LICENSE.txt]: https://github.com/nccgroup/depthcharge/blob/master/LICENSE.txt
-[SPDX Identifier]: https://spdx.dev/ids
-
-"""
 
 setup(
     name='depthcharge',
     version=get_version(),
     description='A U-Boot toolkit for security researchers and tinkerers',
 
-    long_description=_LONG_DESC,
+    long_description=get_description(),
     long_description_content_type='text/markdown',
 
     license='BSD 3-Clause License',

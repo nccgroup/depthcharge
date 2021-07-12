89af3d0c9e37c07de644c835b836a7d5705384db
Jon Szymaniak <jon.szymaniak.foss@gmail.com>
2021-07-11 21:09:49 -0500

    arch: Export depthcharge.arch.AARCH64

--- python/depthcharge/arch/__init__.py
+++ python/depthcharge/arch/__init__.py
@@ -9,4 +9,5 @@ Support for various target architectures
 from .arch import Architecture
 
 from .arm import ARM
+from .aarch64 import AARCH64
 from .generic import Generic, GenericBE, Generic64, Generic64BE
